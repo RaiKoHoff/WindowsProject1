028604d2fb7366d0950feb282c93ee2da832dc05
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2018-11-27 14:03:47 +0100

    initial version

---
+++ WindowsProject1/commonres.h
@@ -0,0 +1,7 @@
+// Verwendet von WindowsProject1.rc
+
+#define IDD_WINDOWSPROJECT1_DIALOG  10001
+#define IDI_WINDOWSPROJECT1         11001
+#define IDC_WINDOWSPROJECT1         12001
+#define IDC_ABOUTBOXTEXT            13001
+#define IDS_ABOUTBOX_TEXT           14001
